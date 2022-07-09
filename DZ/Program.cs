a57553fb1970bd18ccd48c2d907e11810d25b7a3
Aleksey <aleksey-asapov@mail.ru>
2022-07-09 16:07:43 +0300

    Решение ДЗ

--- DZ/Program.cs
+++ DZ/Program.cs
@@ -24,18 +24,35 @@
 // b1 = 2, k1 = 5, b2 = 4, 
 // k2 = 9 -> (-0,5; 5,5)
 
-Console.Write("Введите k1: ");
-var k1 = Convert.ToDouble(Console.ReadLine());
-Console.Write("Введите b1: ");
-var b1 = Convert.ToDouble(Console.ReadLine());
-Console.Write("Введите k2: ");
-var k2 = Convert.ToDouble(Console.ReadLine());
-Console.Write("Введите b2: ");
-var b2 = Convert.ToDouble(Console.ReadLine());
-var x = -(b1 - b2) / (k1 - k2);
-var y = k1 * x + b1;
-x = Math.Round(x, 3);
-y = Math.Round(y, 3);
-Console.WriteLine($"Пересечение в точке: ({x};{y})");
+// Console.Write("Введите k1: ");
+// var k1 = Convert.ToDouble(Console.ReadLine());
+// Console.Write("Введите b1: ");
+// var b1 = Convert.ToDouble(Console.ReadLine());
+// Console.Write("Введите k2: ");
+// var k2 = Convert.ToDouble(Console.ReadLine());
+// Console.Write("Введите b2: ");
+// var b2 = Convert.ToDouble(Console.ReadLine());
+// var x = -(b1 - b2) / (k1 - k2);
+// var y = k1 * x + b1;
+// x = Math.Round(x, 3);
+// y = Math.Round(y, 3);
+// Console.WriteLine($"Пересечение в точке: ({x};{y})");
 
 
+string s = Console.ReadLine();
+string[] nums = s.Split(',');
+
+Console.WriteLine(s);
+
+int[] mass = new int[nums.Length];
+for (int i =0; i < nums.Length; i++)
+{
+    int count = 0;
+     if (arr[i] > 0)
+     {
+         count++;
+     }
+    Console.WriteLine("Подстрока: " + nums[i]);
+    mass[i] = int.Parse(nums[i]);
+    Console.WriteLine();
+}
---
+++ Seminar_09.07.22/Program.cs
@@ -0,0 +1,152 @@
+﻿// // Задача 46: Задайте двумерный массив размером 
+// // m×n, заполненный случайными целыми числами.
+// // m = 3, n = 4. 
+// // 1 4 8 19 
+// // 5 -2 33 -2 
+// // 77 3 8 1
+
+// void Print(int[,] arr)
+// {
+//     for (int i = 0; i < arr.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < arr.GetLength(1); j++)
+//         {
+//             Console.Write(arr[i, j] + ", ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+// int m = 3, n = 4;
+// int[,] mass = new int[m, n];
+
+// for (int i = 0; i < mass.GetLength(0); i++)
+// {
+//     for (int j = 0; j < mass.GetLength(1); j++)
+//     {
+//         mass[i, j] = new Random().Next(-100, 100);
+//     }
+// }
+
+// Print(mass);
+
+// Задача 48: Задайте двумерный массив размера m на n, каждый элемент в массиве находится 
+// по формуле: Aₙₙ = m+n. Выведите полученный массив на экран.
+// m = 3, n = 4. 
+// 0 1 2 3 
+// 1 2 3 4 
+// 2 3 4 5
+
+// void Print(int[,] arr)
+// {
+//     for (int i = 0; i < arr.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < arr.GetLength(1); j++)
+//         {
+//             Console.Write(arr[i, j] + ", ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+// int m = 3, n = 4;
+// int[,] mass = new int[m, n];
+
+// for (int i = 0; i < mass.GetLength(0); i++)
+// {
+//     for (int j = 0; j < mass.GetLength(1); j++)
+//     {
+//         mass[i, j] = i + j;
+//     }
+// }
+// Print(mass);
+
+// Задача 49: Задайте двумерный массив. 
+// Найдите элементы, у которых оба индекса чётные, 
+// и замените эти элементы на их квадраты.
+
+// Например, изначально массив выглядел вот так: 
+// 1 4 7 2 
+// 5 9 2 3 
+// 8 4 2 4
+
+// Новый массив будет выглядеть вот так: 
+// 1 4 7 2 
+// 5 81 2 9 
+// 8 4 2 4
+
+// void Print(int[,] arr)
+// {
+//     for (int i = 0; i < arr.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < arr.GetLength(1); j++)
+//         {
+//             Console.Write(arr[i, j] + ", ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+// int m = 3, n = 4;
+// int[,] mass = new int[m, n];
+
+// for (int i = 0; i < mass.GetLength(0); i++)
+// {
+//     for (int j = 0; j < mass.GetLength(1); j++)
+//     {
+//         mass[i, j] = i + j;
+//     }
+// }
+// Print(mass);
+// Console.WriteLine();
+
+// for (int i = 0; i < mass.GetLength(0); i+=2)
+// {
+//     for (int j = 0; j < mass.GetLength(1); j+=2)
+//     {
+//         mass[i, j] = mass[i, j] * mass[i, j];
+//     }
+// }
+
+// Print(mass);
+
+
+
+// Задача 51: Задайте двумерный массив. 
+// Найдите элементы, у которых оба индекса чётные, 
+// и замените эти элементы на их квадраты.
+
+
+// void Print(int[,] arr)
+// {
+//     for (int i = 0; i < arr.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < arr.GetLength(1); j++)
+//         {
+//             Console.Write(arr[i, j] + ", ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+// int m = 3, n = 4;
+// int[,] mass = new int[m, n];
+
+// for (int i = 0; i < mass.GetLength(0); i++)
+// {
+//     for (int j = 0; j < mass.GetLength(1); j++)
+//     {
+//         mass[i, j] = i + j;
+//     }
+// }
+
+// int sum = 0;
+
+// for (int i = 0; i < Math.Min(mass.GetLength(0), mass.GetLength(1)); i++)
+// {
+//     sum += mass[i, i];
+// }
+
+// Print(mass);
+// Console.WriteLine(sum);
+
