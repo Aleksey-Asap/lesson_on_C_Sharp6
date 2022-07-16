40643b74dfe583ad1ecc212ee2cfba037e99d31e
Aleksey <aleksey-asapov@mail.ru>
2022-07-16 21:30:13 +0300

    Решение ДЗ

--- DZ/Program.cs
+++ DZ/Program.cs
@@ -4,19 +4,24 @@
 // 0, 7, 8, -2, -2 -> 2
 // -1, -7, 567, 89, 223-> 3
 
-// Console.Write("Введите элементы(через пробел): ");
-// int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+// Console.Write("Введите кол-во чисел: ");
+// int M = int.Parse(Console.ReadLine());
 // int count = 0;
- 
+
+// int[] arr = new int[M];
 // for (int i = 0; i < arr.Length; i++)
 // {
+//     Console.Write($"Число {i + 1} = ");
+//     arr[i] = int.Parse(Console.ReadLine());
+
 //     if (arr[i] > 0)
 //     {
 //         count++;
 //     }
 // }
- 
+
 // Console.WriteLine($"Кол-во элементов > 0: {count}");
+
 
 // Задача 43. Напишите программу, которая найдёт 
 // точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения 
@@ -39,20 +44,22 @@
 // Console.WriteLine($"Пересечение в точке: ({x};{y})");
 
 
-string s = Console.ReadLine();
-string[] nums = s.Split(',');
+// Задача 43. Напишите программу, которая найдёт 
+// точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения 
+// b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, 
+// k2 = 9 -> (-0,5; 5,5)
 
-Console.WriteLine(s);
-
-int[] mass = new int[nums.Length];
-for (int i =0; i < nums.Length; i++)
-{
-    int count = 0;
-     if (arr[i] > 0)
-     {
-         count++;
-     }
-    Console.WriteLine("Подстрока: " + nums[i]);
-    mass[i] = int.Parse(nums[i]);
-    Console.WriteLine();
-}
+// Console.Write("Введите k1: ");
+// double k1 = double.Parse(Console.ReadLine());
+// Console.Write("Введите b1: ");
+// double b1 = double.Parse(Console.ReadLine()); // double.Parse
+// Console.Write("Введите k2: ");
+// double k2 = double.Parse(Console.ReadLine());
+// Console.Write("Введите b2: ");
+// double b2 = double.Parse(Console.ReadLine());
+// double x = -(b1 - b2) / (k1 - k2);
+// double y = k1 * x + b1;
+// x = Math.Round(x, 2);
+// y = Math.Round(y, 2);
+// Console.WriteLine($"Пересечение в точке: ({x};{y})");
