dd4b462b87851c011b77b4ccf1febdea31ce2c69
Aleksey <aleksey-asapov@mail.ru>
2022-07-16 21:32:45 +0300

    Решение задач 41, 43

--- DZ_09.07.22/Program.cs
+++ DZ_09.07.22/Program.cs
@@ -40,3 +40,117 @@
 // 5 9 2 3 
 // 8 4 2 4
 // 17 -> такого числа в массиве нет
+
+// void Print(int[,] arr)
+// {
+//     for (int i = 0; i < arr.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < arr.GetLength(1); j++)
+//         {
+//             Console.Write(arr[i, j] + ", ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+// int m = 3, n = 4;
+// int[,] mass = new int[m, n];
+
+// for (int i = 0; i < mass.GetLength(0); i++)
+// {
+//     for (int j = 0; j < mass.GetLength(1); j++)
+//     {
+//         mass[i, j] = i + j;
+//     }
+// }
+// Print(mass);
+// Console.WriteLine();
+
+// Console.WriteLine("Введите первое число массива:");
+// int k = int.Parse(Console.ReadLine());
+// Console.WriteLine("Введите введите второе число массива:");
+// int t = int.Parse(Console.ReadLine());
+// int[,] mass1 = new int[k, t];
+
+// for (int i = 0; i < mass.GetLength(0); i++)
+// {
+//     for (int j = 0; j < mass.GetLength(1); i++)
+//     {
+//         if (mass[i, j] == mass1[k, t])
+//         {
+//             Console.WriteLine(mass[i, j]);
+//         }
+//         else Console.WriteLine("Такого значения нет");
+//     }
+// }
+
+// Print(mass);
+
+
+
+
+
+
+
+
+
+void Print(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i, j] + ", ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int m = 3, n = 4;
+int[,] mass = new int[m, n];
+
+for (int i = 0; i < mass.GetLength(0); i++)
+{
+    for (int j = 0; j < mass.GetLength(1); j++)
+    {
+        mass[i, j] = i + j;
+    }
+}
+
+double sum1 = 0;
+double sum2 = 0;
+double sum3 = 0;
+double sum4 = 0;
+double ArithmeticSum1 = 0;
+double ArithmeticSum2 = 0;
+double ArithmeticSum3 = 0;
+double ArithmeticSum4 = 0;
+
+for (int i = 0; i < mass.GetLength(0); i++)
+{
+
+    sum1 += mass[i, 0];
+    ArithmeticSum1 = sum1 / m;
+    sum2 += mass[i, 1];
+    ArithmeticSum2 = sum2 / m;
+    sum3 += mass[i, 2];
+    ArithmeticSum3 = sum3 / m;
+    sum4 += mass[i, 3];
+    ArithmeticSum4 = sum4 / m;
+}
+
+Print(mass);
+Console.WriteLine(" Cреднее арифметическое элементов в каждом столбце:");
+Console.WriteLine($"{ArithmeticSum1}, {ArithmeticSum2}, {ArithmeticSum3}, {ArithmeticSum4}");
+
+
+
+// for (int i=1,k=0;i<5;i+=2) 
+// {
+//     double summa=0;
+//     for (int j=0;j<5;j++) 
+//     {
+//         summa+=a[i,j];
+//     }
+//     b[k]=summa/5;
+//     Console.WriteLine("Среднее арифметическое столбца "+(i+1)+" равно: "+b[k]);
---
+++ Seminar_10.07.22/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 53: Задайте двумерный массив. Напишите программу, 
+// которая поменяет местами первую и последнюю строку массива.
+
+
+void Print(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i, j] + ", ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int m = 3, n = 4;
+int[,] mass = new int[m, n];
+
+for (int i = 0; i < mass.GetLength(0); i++)
+{
+    for (int j = 0; j < mass.GetLength(1); j++)
+    {
+        mass[i, j] = i + j;
+    }
+}
+
+int sum = 0;
+
+for (int i = 0; i < Math.Min(mass.GetLength(0), mass.GetLength(1)); i++)
+{
+    sum += mass[i, i];
+}
+
+Print(mass);
+Console.WriteLine(sum);
+
